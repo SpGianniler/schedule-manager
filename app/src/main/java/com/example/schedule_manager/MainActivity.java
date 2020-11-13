11f193382a1fb26b3273d2a814cc1cf15e63cced
Basilis-Kalogiroy <73667650+Basilis-Kalogiroy@users.noreply.github.com>
2020-11-13 23:24:54 +0200

    Basic UI Functionality
    Early stages of UI.Added a couple of activities as well as menu bars.

--- app/src/main/java/com/example/schedule_manager/MainActivity.java
+++ app/src/main/java/com/example/schedule_manager/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.schedule_manager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
 
+
+
+
+public class MainActivity extends BaseActivity {
+    private Button userLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userLoginButton = (Button) findViewById(R.id.userButton);
+        userLoginButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               openActivityULS();
+           }
+         });
     }
+
+    public void openActivityULS(){
+        Intent intent = new Intent(this,UserLoginScreen.class);
+        startActivity(intent);
+    }
+
+
+
+
+
 }
