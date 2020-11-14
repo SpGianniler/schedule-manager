cfb2f010fec196f3be8d782d983ede4b53fa93ec
SpGianniler <45335796+SpGianniler@users.noreply.github.com>
2020-11-14 23:10:22 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/schedule_manager/MainActivity.java
+++ app/src/main/java/com/example/schedule_manager/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         List<Ergazomenoi> ergazomenoiList = new ArrayList<>();
                 super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
