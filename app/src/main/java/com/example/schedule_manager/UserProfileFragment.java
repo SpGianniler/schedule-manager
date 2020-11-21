2047506a2b60ff80df757c670bd77ca00e28978c
XrhstosTerzenidhs <63361266+XrhstosTerzenidhs@users.noreply.github.com>
2020-11-21 16:29:25 +0200

    changes of user welcome

--- app/src/main/java/com/example/schedule_manager/UserProfileFragment.java
+++ app/src/main/java/com/example/schedule_manager/UserProfileFragment.java
@@ -16,18 +16,43 @@ import androidx.fragment.app.Fragment;
 
 public class UserProfileFragment extends Fragment {
 
+    private ImageButton imagebtn;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // create ContextThemeWrapper from the original Activity Context with the custom theme
+        //return inflater.inflate(R.layout.user_profile_fragment, container, false);
+        /*// create ContextThemeWrapper from the original Activity Context with the custom theme
         final Context contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.Theme_MaterialComponents_DayNight_DarkActionBar);
 
         // clone the inflater using the ContextThemeWrapper
         LayoutInflater localInflater = inflater.cloneInContext(contextThemeWrapper);
 
         // inflate the layout using the cloned inflater, not default inflater
-        return localInflater.inflate(R.layout.user_profile_fragment, container, false);
+        return inflater.inflate(R.layout.user_profile_fragment, container, false);*/
+
+
+        View view = inflater.inflate(R.layout.user_profile_fragment, container, false);
+
+        imagebtn = (ImageButton) view.findViewById(R.id.settingsBTN);
+
+        imagebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivitySettings();
+            }
+        });
+
+        return view;
 
     }
+
+
+    public void openActivitySettings(){
+        Intent intent = new Intent((getActivity()), UserProfileSettings.class);
+        startActivity(intent);
+    }
+
+
 
 }
