76e1b54f56f5cae6a70835ca238f36ea19ca9e59
XrhstosTerzenidhs <63361266+XrhstosTerzenidhs@users.noreply.github.com>
2020-11-21 05:25:57 +0200

    changes of user welcome

--- app/src/main/java/com/example/schedule_manager/UserProfileFragment.java
+++ app/src/main/java/com/example/schedule_manager/UserProfileFragment.java
@@ -1,11 +1,14 @@
 package com.example.schedule_manager;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,7 +19,15 @@ public class UserProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.user_profile_fragment, container, false); //checkit
+        // create ContextThemeWrapper from the original Activity Context with the custom theme
+        final Context contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.Theme_MaterialComponents_DayNight_DarkActionBar);
+
+        // clone the inflater using the ContextThemeWrapper
+        LayoutInflater localInflater = inflater.cloneInContext(contextThemeWrapper);
+
+        // inflate the layout using the cloned inflater, not default inflater
+        return localInflater.inflate(R.layout.user_profile_fragment, container, false);
 
     }
+
 }
