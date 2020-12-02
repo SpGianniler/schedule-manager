0f20ac627914d5c1f4c5acf3e3e29dc2652673f2
apostolos <valiakosapostolos@hotmail.com>
2020-12-02 16:29:48 +0200

    Added Stuff For test reasons

--- app/src/main/java/com/example/schedule_manager/Matrix.java
+++ app/src/main/java/com/example/schedule_manager/Matrix.java
@@ -62,48 +62,48 @@ public class Matrix {
         return totalHours;
     }
 
-    public void addHours(String vardia){
-        if(vardia.equals("6")){
+        public void addHours(String vardia){
+            if(vardia.equals("6")){
+                if(this.ergazomenos.contract.equals("full"))
+                    this.wres6 +=8;
+                else
+                    this.wres6+=4;
+            }
+            else if(vardia.equals("1")){
+                if(this.ergazomenos.contract.equals("full"))
+                    this.wres1 +=8;
+                else
+                    this.wres1+=4;
+            }
+            else if(vardia.equals("2")){
+                if(this.ergazomenos.contract.equals("full"))
+                    this.wres2 +=8;
+                else
+                    this.wres2+=4;
+            }
+            else if(vardia.equals("3")){
+                if(this.ergazomenos.contract.equals("full"))
+                    this.wres3 +=8;
+                else
+                    this.wres3+=4;
+            }
+            else if(vardia.equals("4")){
+                if(this.ergazomenos.contract.equals("full"))
+                    this.wres4+=8;
+                else
+                    this.wres4+=4;
+            }
+            else if(vardia.equals("5")){
+                if(this.ergazomenos.contract.equals("full"))
+                    this.wres5 +=8;
+                else
+                    this.wres5+=4;
+            }
             if(this.ergazomenos.contract.equals("full"))
-                this.wres6 +=8;
+                this.totalHours+=8;
             else
-                this.wres6+=4;
+                this.totalHours+=4;
         }
-        else if(vardia.equals("1")){
-            if(this.ergazomenos.contract.equals("full"))
-                this.wres1 +=8;
-            else
-                this.wres1+=4;
-        }
-        else if(vardia.equals("2")){
-            if(this.ergazomenos.contract.equals("full"))
-                this.wres2 +=8;
-            else
-                this.wres2+=4;
-        }
-        else if(vardia.equals("3")){
-            if(this.ergazomenos.contract.equals("full"))
-                this.wres3 +=8;
-            else
-                this.wres3+=4;
-        }
-        else if(vardia.equals("4")){
-            if(this.ergazomenos.contract.equals("full"))
-                this.wres4+=8;
-            else
-                this.wres4+=4;
-        }
-        else if(vardia.equals("5")){
-            if(this.ergazomenos.contract.equals("full"))
-                this.wres5 +=8;
-            else
-                this.wres5+=4;
-        }
-        if(this.ergazomenos.contract.equals("full"))
-            this.totalHours+=8;
-        else
-            this.totalHours+=4;
-    }
 
 
     public int getHours(String vardia){
@@ -150,10 +150,10 @@ public class Matrix {
         }
     }
 
-    public void remHours() {
-        if(this.ergazomenos.contract.equals("full"))
-            this.ergazomenos.evWres -=8;
-        else
-            this.ergazomenos.evWres-=4;
-    }
+        public void remHours() {
+            if(this.ergazomenos.contract.equals("full"))
+                this.ergazomenos.evWres -=8;
+            else
+                this.ergazomenos.evWres-=4;
+        }
 }
