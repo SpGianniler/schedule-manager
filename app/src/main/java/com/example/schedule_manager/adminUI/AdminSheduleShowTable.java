ae9bd5ea30ead2a7b84a99151207fbd61a886b54
XrhstosTerzenidhs <63361266+XrhstosTerzenidhs@users.noreply.github.com>
2020-12-13 17:46:53 +0200

    commit ui

---
+++ app/src/main/java/com/example/schedule_manager/adminUI/AdminSheduleShowTable.java
@@ -0,0 +1,15 @@
+package com.example.schedule_manager.adminUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.schedule_manager.R;
+
+public class AdminSheduleShowTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_shedule_show_table_activity);
+    }
+}
