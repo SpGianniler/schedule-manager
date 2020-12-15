1369fc158cbdd3a0dd3f889c18cf760c5a5c468c
Basilis-Kalogiroy <73667650+Basilis-Kalogiroy@users.noreply.github.com>
2020-12-15 19:13:17 +0200

    Added Admin Settings too

--- app/src/main/java/com/example/schedule_manager/adminUI/AdminProfileFragment.java
+++ app/src/main/java/com/example/schedule_manager/adminUI/AdminProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.schedule_manager.adminUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.schedule_manager.R;
+import com.example.schedule_manager.userUI.UserProfileSettings;
 
 public class AdminProfileFragment extends Fragment {
     protected TextView username, specialty;
+    private ImageButton settingsButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,8 +28,18 @@ public class AdminProfileFragment extends Fragment {
 
         specialty= (TextView) view.findViewById(R.id.Admin_Specialty);
         specialty.append(" "+AdminLoginActivity.getEidikotita());
+        settingsButton=(ImageButton) view.findViewById(R.id.settingsBTN);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivitySettings();
+            }
+        });
         return view;
     }
 //TODO: na kanw kai stis upoloipess klaseis na epistrefei view kai oxi inflater.inflate(R....)
-
+    public void openActivitySettings(){
+        Intent intent = new Intent((getActivity()), AdminProfileSettings.class);
+        startActivity(intent);
+    }
 }
